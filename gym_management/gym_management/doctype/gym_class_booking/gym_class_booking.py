99997417557132fafc02ccd5f5f71e0937631097
sydney2323 <sydneykb38@gmail.com>
2023-02-18 11:31:06 +0300

    feat: checking user membership well as activeness

--- gym_management/gym_management/doctype/gym_class_booking/gym_class_booking.py
+++ gym_management/gym_management/doctype/gym_class_booking/gym_class_booking.py
@@ -1,8 +1,27 @@
 # Copyright (c) 2023, sydney and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from datetime import date
 from frappe.model.document import Document
 
 class GymClassBooking(Document):
-	pass
+	def before_save(self):
+
+		today_date = date.today()
+		user = frappe.session.user
+
+		# checking user membership well as activeness
+		check_membership(user, today_date)
+
+
+def check_membership(user, today_date):
+	exists = frappe.db.exists(
+	"Gym Membership",
+		{
+		"gym_member": user,
+		"to_date": ('<', today_date),
+		},
+	)
+	if exists:
+		frappe.throw('Your not an active member in our Gym. Please contact the admin to give you Membership')
