3ec03f3b8683aaa18333016a1ac9c2ccefd71353
sydney2323 <sydneykb38@gmail.com>
2023-02-18 05:17:42 +0300

    feat: checking active membership

--- gym_management/gym_management/doctype/gym_membership/gym_membership.py
+++ gym_management/gym_management/doctype/gym_membership/gym_membership.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2023, sydney and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class GymMembership(Document):
-	pass
+    # check before submitting this document
+    def before_save(self):
+        exists = frappe.db.exists(
+            'Gym Membership',
+            {
+                'gym_member': self.gym_member,
+                # check if the membership's end date is later than this membership's start date
+                'to_date': ('>', self.from_date),
+            },
+        )
+        if exists:
+            frappe.throw('There is an active membership for this member')
+
