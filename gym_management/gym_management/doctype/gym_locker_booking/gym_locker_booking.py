e137a8ed2283d03f625bd09bebddcfe31b604047
sydney2323 <sydneykb38@gmail.com>
2023-02-18 06:09:20 +0300

    feat: checking user membership well as activeness

--- gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.py
+++ gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.py
@@ -3,9 +3,18 @@
 
 import frappe
 from frappe.model.document import Document
+from datetime import date
 
 class GymLockerBooking(Document):
 	def before_save(self):
+
+		today_date = date.today()
+		user = frappe.session.user
+
+		# checking user membership well as activeness
+		check_membership(user, today_date)
+
+		# checking the has been boocked
 		exists = frappe.db.exists(
 		"Gym Locker Booking",
 			{
@@ -14,3 +23,15 @@ class GymLockerBooking(Document):
 		)
 		if exists:
 		    frappe.throw(f'{self.locker} is already boooked')
+
+def check_membership(user, today_date):
+	exists = frappe.db.exists(
+	"Gym Membership",
+		{
+		"gym_member": user,
+		"to_date": ('<', today_date),
+		},
+	)
+	if exists:
+		frappe.throw('Your not an active member in our Gym. Please contact the admin to give you Membership')
+
